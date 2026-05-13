2f86d2885f3cae3389c374fbbf565c82714d1efd
Rayhan-Rakib1 <rayhanrakib448@gmail.com>
2026-05-13 23:24:03 +0600

    surah updated

--- src/app/(commonLayout)/surahs/page.tsx
+++ src/app/(commonLayout)/surahs/page.tsx
@@ -147,7 +147,7 @@ const SurahsPage = () => {
               </div>
 
               <span className="text-xs text-gray-500">
-                {s.ayahs} Ayahs
+                {s.ayahs} Ayahss
               </span>
             </Link>
           ))}
